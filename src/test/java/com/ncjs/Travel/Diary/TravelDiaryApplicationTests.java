57d740da288b336e18c96e2aa3990555c8eec1e6
Ariel Mitchell <ariel.l.makowski@gmail.com>
2023-12-07 21:56:35 -0500

    project framework

---
+++ src/test/java/com/ncjs/Travel/Diary/TravelDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ncjs.Travel.Diary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
