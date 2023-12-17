862183cb87ccabd9f2f2e64ad532de2a43f7bf3f
Nidia Menchaca <nmenchaca@Nidias-MacBook-Air.local>
2023-12-17 16:31:11 -0600

    started implementing first feature

--- src/main/java/com/ncjs/Travel/Diary/controllers/TripFolders.java
+++ src/main/java/com/ncjs/Travel/Diary/controllers/TripFolders.java
@@ -1,4 +1,61 @@
 package com.ncjs.Travel.Diary.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
 public class TripFolders {
-}
+
+//  private static int nextId = 6;
+  private static final Map< Integer, String > trips = new HashMap<>() {{
+      put(1,"San Francisco");
+      put(2,"Italy");
+      put(3,"Greece");
+      put(4,"Paris");
+      put(5,"New York");
+  }};
+
+    @GetMapping("/")
+    @ResponseBody
+    public String displayHomePage() {
+        StringBuilder tripsList = new StringBuilder();
+        for(int tripId: trips.keySet()) {
+            String trip = trips.get(tripId);
+            tripsList.append("<li><a href ='/trips'").append(tripId).append("'>").append(trip).append("</a>");
+        }
+
+
+        return "<h1> Travel Diary </h1>" +
+                "<h1> Trips </h1>" +
+                "<ul>" +
+                tripsList +
+                "</ul>" +
+                "<h2> + Add new <a href='/add-trips'>trip</a> </h2>";
+    }
+
+    @GetMapping("/add-trips")
+    @ResponseBody
+    public String addTripPage() {
+    return "<h2> Add Trip title </h2>" +
+           "<h2> Choose a cover photo! </h2>" +
+           "<h2> Add description </h2>" +
+           "<h2> + Add new photo </h2>";
+
+
+    }
+
+//    @GetMapping("/add")
+//    @ResponseBody
+//    public String addTrips(@RequestParam String trip) {
+//        trips.put(nextId,trip);
+//        nextId++;
+//        return "<p> You have added a trip" + trip + "to this </p>";
+    }
+
+
+
