03c1342957c3892f7bda9959fdd8728881f6281c
sdale0321 <67200326+sdale0321@users.noreply.github.com>
2023-12-18 13:31:41 -0600

    adding React for locationSearch

--- src/main/resources/js/searchWindow.js
+++ src/main/resources/js/searchWindow.js
@@ -1,3 +1,29 @@
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [locations, setLocations] = useState([]);
+  const [newLocation, setNewLocation] = useState('');
+
+  useEffect(() => {
+    fetch('/api/locations')
+      .then(response => response.json())
+      .then(data => setLocations(data));
+  }, []);
+
+  const handleAddLocation = () => {
+    fetch('/api/locations', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ name: newLocation }),
+    })
+      .then(response => response.json())
+      .then(data => setLocations([...locations, data]));
+
+    setNewLocation('');
+  };
+
 const addButton = document.getElementById('addButton');
 const modalOverlay = document.getElementById('modalOverlay');
 const locationModal = document.getElementById('locationModal');
