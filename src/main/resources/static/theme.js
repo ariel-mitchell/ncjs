ca5490c2c775c306761f3e82700257fa19d7e5cd
Nidia <giglla98@hotmail.com>
2024-01-13 23:46:38 -0800

    fixed light and dark mode option

--- src/main/resources/static/theme.js
+++ src/main/resources/static/theme.js
@@ -1,3 +1,34 @@
+//function changeTheme() {
+//    document.body.classList.toggle('dark-mode');
+//
+//    let newThemePreference = document.body.classList.contains('dark-mode') ? 'dark' : 'light';
+//    localStorage.setItem('themePreference', newThemePreference);
+//}
+//
+//document.addEventListener("DOMContentLoaded", function() {
+//    let userThemePreference = localStorage.getItem('themePreference');
+//
+//    if (userThemePreference === 'dark') {
+//        document.body.classList.add('dark-mode');
+//    }
+//
+//
+//     let themeChangeButton = document.getElementById('theme-change');
+//        if (themeChangeButton) {
+//            themeChangeButton.addEventListener('click', changeTheme);
+//        }
+//});
+
+
+// theme.js
+document.addEventListener("DOMContentLoaded", function () {
+    let userThemePreference = localStorage.getItem('themePreference');
+
+    if (userThemePreference === 'dark') {
+        document.body.classList.add('dark-mode');
+    }
+});
+
 function changeTheme() {
     document.body.classList.toggle('dark-mode');
 
@@ -5,16 +36,4 @@ function changeTheme() {
     localStorage.setItem('themePreference', newThemePreference);
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    let userThemePreference = localStorage.getItem('themePreference');
-
-    if (userThemePreference === 'dark') {
-        document.body.classList.add('dark-mode');
-    }
-
-
-     let themeChangeButton = document.getElementById('theme-change');
-        if (themeChangeButton) {
-            themeChangeButton.addEventListener('click', changeTheme);
-        }
-});
+console.log("hello");
